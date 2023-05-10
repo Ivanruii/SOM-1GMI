ebc94088aa74013ec14c72db26fc0acdf6814a76
Ivanruii <ivr.644@gmail.com>
2023-05-10 14:40:39 +0200

    Nuevos ejercicio

--- batch/act3ivan/batch1ivan.cmd
+++ batch/act3ivan/batch1ivan.cmd
@@ -1,10 +1,10 @@
 @echo off
 
 set /p nota=Ingresa la nota:
+set /a "restante= 5 - %nota%"
 
 if %nota% GEQ 5 (
     echo Felicidades, has aprobado.
 ) else (
-    set /a "restante= 5 - %nota% - 1"
     echo Has suspendido, te han faltado %restante% puntos para aprobar.
 )
